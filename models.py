f4b8c2f1e9a890fb9ac31c80cf11b1d3ec4f8c92
Wolodaa <128287349+Wolodaa@users.noreply.github.com>
2023-06-09 22:15:31 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,30 +1,4 @@
-from app import db, login_manager
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-
-class User(db.Model, UserMixin):
-    __tablename__ = "user" # ім'я таблиці
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    
-    orders = db.relationship('Order', backref='order', lazy=True)
-
-    #Метод repr використовується для відображення об'єкта у зручному для читача форматі.
-    def __repr__(self):
-        return f"User: {self.username}"
-
-    def set_password(self, original_password):
-        self.password = generate_password_hash(original_password)
-
-    def check_password(self, original_password):
-        return check_password_hash(self.password, original_password)
-   
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+from app import db
 
 class Pizza(db.Model):
     __tablename__ = "pizza"
@@ -37,7 +11,11 @@ class Pizza(db.Model):
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String)
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    amount = db.Column(db.Integer, default = 1)
+    phone = db.Column(db.String)
+    city =  db.Column(db.String)
     address = db.Column(db.String)
+    comment =  db.Column(db.Text)
     status = db.Column(db.String)
